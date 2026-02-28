59a0a686037142fc94fb1085470469d5482ac80c
Rodney Dejournett <rdjn@Rodneys-MacBook-Air.local>
2026-02-27 23:44:01 -0500

    inc1: fix deprecated libs passlib and time.UTC

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,11 @@
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
+# from passlib.context import CryptContext
+import bcrypt
+from datetime import datetime, timedelta, timezone
 from jose import jwt
 
 #Passing hashing
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 #JWT settings  (TODO: move these settings to env vars)
 
@@ -14,14 +15,18 @@ ACESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(bytes(password, encoding="utf-8"),
+                         bcrypt.gensalt(),
+                         )
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(bytes(plain_password, encoding="utf-8"),
+                         hashed_password,
+                          )
 
 def create_acess_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(datetime.UTC) + timedelta(minutes=ACESS_TOKEN_EXPIRE_MINUTES) 
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACESS_TOKEN_EXPIRE_MINUTES) 
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes import auth
 
-app = FastAPI(title="Persoanl Bank Aggregator")
+app = FastAPI(title="Personal Bank Aggregator")
 
 app.include_router(auth.router)
 
