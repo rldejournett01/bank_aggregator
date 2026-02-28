afe222441493629c250b4171eb409b40ddc280ef
Rodney Dejournett <rldejournett01@gmail.com>
2026-02-28 10:26:21 -0500

    inc3: auto create tables in main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.routes import auth
+from app.core.database import engine
+from app.models import user
+
+user.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Personal Bank Aggregator")
 
