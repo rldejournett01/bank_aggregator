fe46906ed0bbf0aa45e06e59f54beae730b5b26b
Rodney Dejournett <rldejournett01@gmail.com>
2026-02-28 10:25:24 -0500

    inc2: created user orm

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+import uuid 
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime, timezone
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    
