79826eabc903d857e546d7e9eff3624d993e69a4
Rodney Dejournett <rldejournett01@gmail.com>
2026-02-28 10:24:15 -0500

    inc2: db config layer

---
+++ app/core/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://localhost/bank_aggregator"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
