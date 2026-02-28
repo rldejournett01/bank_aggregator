34a1aa6763809e1ba6658b6aa94859ba96bc2c84
Rodney Dejournett <rldejournett01@gmail.com>
2026-02-28 11:33:18 -0500

    inc3: updating auth router to use db

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+
 from app.schemas.user import UserCreate, UserLogin, Token
 from app.core.security import hash_password, verify_password,create_acess_token
+from app.core.deps import get_db
+from app.models.user import User
 
 
 #Router
@@ -9,30 +13,42 @@ router = APIRouter(prefix="/auth", tags=["Authentication"])
 #TEMP storage (will be moved to DB)
 fake_users_db = {}
 
-#Sign Up Route
+#Sign Up Route with dependency injection
 @router.post("/signup")
-def signup(user: UserCreate):
-    if user.email in fake_users_db:
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+
+    #Check if existing
+    #Sends query to User database and filters for exact email, returns first result
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
         raise HTTPException(status_code=400, detail="User already exists")
     
-    fake_users_db[user.email] = {
-        "email": user.email,
-        "hash_password": hash_password(user.password)
-    }
+    #create new User
+    new_user = User(
+        email=user.email,
+        hash_password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
 
     return {"message": "User created successfully"}
 
-#Login Route
+#Login Route with dependency injection
 @router.post("/login", response_model=Token)
-def login(user: UserLogin):
-    db_user = fake_users_db.get(user.email)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    #send query to db to find the user, returns first result
+    db_user = db.query(User).filter(User.email == user.email).first()
+
 
     if not db_user:
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
-    if not verify_password(user.password, db_user["hash_password"]):
+    if not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
-    token = create_acess_token({"sub": user.email})
+    token = create_acess_token({"sub": str(db_user.id)})
 
     return {"access_token": token, "token_type": "bearer"}
